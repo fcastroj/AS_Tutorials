4fdeb58f1993dd431fee85770c9bfe680744edf3
Felipe Castro Jaimes <124801850+fcastroj@users.noreply.github.com>
2025-07-23 08:25:27 -0500

    Part B Completed

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import TemplateView
 
+# Create your views here.
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView): 
+    template_name = 'pages/about.html' 
+ 
+     
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs) 
+        context.update({ 
+            "title": "About us - Online Store", 
+            "subtitle": "About us", 
+            "description": "This is an about page ...", 
+            "author": "Developed by: Your Name", 
+        }) 
+ 
+        return context 
+    
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact us - Online Store",
+            "subtitle": "Contact us",
+            "description": "email: contacus@email.com",
+            "author": "address: Calle 123 # 45-67 / Phone: +57 123 4567890",
+        })
+
+        return context
