be575f17091043c568c95f876bcd2e224dc46c2d
Felipe Castro Jaimes <124801850+fcastroj@users.noreply.github.com>
2025-08-27 18:54:11 -0500

    Section B Finished

--- todoapp/todo/admin.py
+++ todoapp/todo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin 
-from .models import ToDo 
+from .models import Todo 
  
 # Register your models here. 
-admin.site.register(ToDo) 
+admin.site.register(Todo) 
--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class ToDo(models.Model):
+class Todo(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField(blank=True)
     # Set to current time
