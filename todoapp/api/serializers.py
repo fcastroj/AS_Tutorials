ea4f396f62b6e4798bab7979681827e6edb8f33b
Felipe Castro Jaimes <124801850+fcastroj@users.noreply.github.com>
2025-08-27 19:10:59 -0500

    Section D Completed

--- todoapp/api/serializers.py
+++ todoapp/api/serializers.py
@@ -1,5 +1,11 @@
+
 from rest_framework import serializers
 from todo.models import Todo
+
+class TodoToggleCompleteSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Todo
+    fields = ['completed']
 
 class TodoSerializer(serializers.ModelSerializer):
  created = serializers.ReadOnlyField()
@@ -7,3 +13,8 @@ class TodoSerializer(serializers.ModelSerializer):
  class Meta:
     model = Todo
     fields = ['id', 'title', 'memo', 'created', 'completed']
+
+class ToDoSerializer(serializers.ModelSerializer): 
+ class Meta: 
+   model = Todo 
+   fields = ['title', 'memo', 'created', 'completed']
